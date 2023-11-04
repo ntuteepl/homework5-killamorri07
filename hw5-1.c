79b9c55215ee8d53a27f5d3a7b8499af95fb741f
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-11-04 23:37:58 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float celsius;
+    double fahrenheit;
+
+    // Prompt the user for input
+    printf("Enter the temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    // Convert Celsius to Fahrenheit
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    // Display the result with one decimal point and padded zeros
+    printf("Temperature in Fahrenheit: %.1lf\n", fahrenheit);
+
+    return 0;
+}
 
