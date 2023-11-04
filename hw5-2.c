8f99d8d05156451f278323ae23991830700f98f2
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-11-05 00:41:24 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int totalMinutes;
+    double rate = 0.9;  // Rate per minute
+    double totalFee;
+
+    // Prompt the user for input
+    // printf("Enter the total number of minutes: ");
+    scanf("%d", &totalMinutes);
+
+    // Calculate the total fee
+    if (totalMinutes <= 800) {
+        totalFee = totalMinutes * rate;
+    } else if (totalMinutes <= 1500) {
+        totalFee = totalMinutes * rate * 0.9; // 10% discount for all minutes
+    } else {
+        totalFee = totalMinutes * rate * 0.79; // 21% discount for all minutes
+    }
+
+    // Display the result with one decimal point
+    printf("%.1lf\n", totalFee);
+
+    return 0;
+}
 
