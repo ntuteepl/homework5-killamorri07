207076c948392b4984c04cef2d8b9a4927882b67
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-11-07 10:19:15 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+
+void calculateDegrees(int hours, int minutes, double *hoursDegree, double *minutesDegree) {
+    *hoursDegree = (360 / 12) * hours;
+    *hoursDegree += (360 / 12) * (minutes / 60.0);  // Adjust for minutes
+    *minutesDegree = (360 / 60) * minutes;
+}
+
+int main() {
+    int hours, minutes;
+    double hoursDegree, minutesDegree;
+
+    // Prompt the user for input
+    // printf("Enter the time in 12-hour format (HH:MM): ");
+    scanf("%d %d", &hours, &minutes);
+
+    if ((hours >= 1 && hours <= 12) && (minutes >= 0 && minutes <= 59)) {
+        calculateDegrees(hours, minutes, &hoursDegree, &minutesDegree);
+        
+        double totalDegrees = hoursDegree - minutesDegree; // The total degrees between the hands
+
+        if (totalDegrees < 0) {
+            totalDegrees = -totalDegrees; // Ensure it's positive
+        }
+
+        if (totalDegrees > 180.0) {
+            double total = totalDegrees - 180.0;
+            printf(" %.3lf\n", total);
+        } 
+        else {
+            printf(" %.3lf\n", totalDegrees);
+        }
+    } else {
+        printf("Invalid input. Please enter a valid time in the format HH:MM.\n");
+    }
+
+    return 0;
+}
 
